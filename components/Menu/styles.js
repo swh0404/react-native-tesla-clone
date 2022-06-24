9a4e51af935bdbd27f142aa7ed2a52170e9f9641
sowahei <ivan.so@mykpi.com>
2022-06-24 17:50:50 +0800

    first commit

---
+++ components/Menu/styles.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  icon: {
+    color: "white",
+  },
+  menuText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginLeft: 15,
+    textTransform: "uppercase",
+  },
+  menuRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  menuTextBox: {
+    flexGrow: 1,
+  },
+  menuItems: {
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  arrowIcon: {
+    color: "#4D4D4E",
+  },
+  subTitle: {
+    color: "grey",
+    fontSize: 14,
+    marginLeft: 15,
+  },
+});
+
+export default styles;
